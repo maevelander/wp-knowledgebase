69c59c177a3ac4ec74f50789fed15180a8ecf00c
Base29 <faisal@base29.com>
2015-05-18 18:26:18 +0500

    * Breadcrumbs text issue fixed. * Added support for Sub-Categories. * Added support for child theme. * Added support for multi site. * Some Code Correction. * Added support for revisions.

--- kbe_settings.php
+++ kbe_settings.php
@@ -9,6 +9,7 @@
             <h2><?php _e('Knowledgebase Display Settings','kbe')?></h2>
             <?php
                 global $wpdb;
+                
                 $tbl_posts = $wpdb->prefix."posts";
                 
                 if(isset($_GET['settings-updated'])){
@@ -52,7 +53,7 @@
                                     <label><?php _e('Knowledgebase Slug','kbe'); ?></label>
                                 </td>
                                 <td colspan="3">
-                                <input type="text" name="kbe_plugin_slug" id="kbe_plugin_slug" value="<?php echo get_option('kbe_plugin_slug'); ?>">
+                                    <input type="text" name="kbe_plugin_slug" id="kbe_plugin_slug" value="<?php echo get_option('kbe_plugin_slug'); ?>">
                                 </td>
                             </tr>
                             <tr>
@@ -60,10 +61,10 @@
                                     <label><?php _e('Number of articles to show','kbe'); ?></label>
                                 </td>
                                 <td colspan="3">
-                                <input type="text" name="kbe_article_qty" id="kbe_article_qty" value="<?php echo get_option('kbe_article_qty'); ?>">
+                                    <input type="text" name="kbe_article_qty" id="kbe_article_qty" value="<?php echo get_option('kbe_article_qty'); ?>">
                                 <p>
-                                	<strong><?php _e('Note:','kbe'); ?></strong>
-									<?php _e('Set the number of articles to show in each category on KB homepage','kbe'); ?>
+                                    <strong><?php _e('Note:','kbe'); ?></strong>
+                                    <?php _e('Set the number of articles to show in each category on KB homepage','kbe'); ?>
                               	</p>
                                 </td>
                             </tr>
@@ -72,12 +73,12 @@
                                     <label><?php _e('Knowledgebase search','kbe'); ?></label>
                                 </td>
                                 <td width="15%">
-                                <input type="radio" name="kbe_search_setting" id="kbe_search_setting" value="1" <?php if(get_option('kbe_search_setting')=='1'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('On','kbe'); ?></span>
+                                    <input type="radio" name="kbe_search_setting" id="kbe_search_setting" value="1" <?php if(get_option('kbe_search_setting')=='1'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('On','kbe'); ?></span>
                                 </td>
                                 <td width="15%">
-                                <input type="radio" name="kbe_search_setting" id="kbe_search_setting" value="0" <?php if(get_option('kbe_search_setting')=='0'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Off','kbe'); ?></span>
+                                    <input type="radio" name="kbe_search_setting" id="kbe_search_setting" value="0" <?php if(get_option('kbe_search_setting')=='0'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Off','kbe'); ?></span>
                                 </td>
                                 <td width="45%">&nbsp;</td>
                             </tr>
@@ -86,30 +87,30 @@
                                     <label><?php _e('Knowledgebase breadcrumbs','kbe'); ?></label>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_breadcrumbs_setting" id="kbe_breadcrumbs_setting" value="1" <?php if(get_option('kbe_breadcrumbs_setting')=='1'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('On','kbe'); ?></span>
+                                    <input type="radio" name="kbe_breadcrumbs_setting" id="kbe_breadcrumbs_setting" value="1" <?php if(get_option('kbe_breadcrumbs_setting')=='1'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('On','kbe'); ?></span>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_breadcrumbs_setting" id="kbe_breadcrumbs_setting" value="0" <?php if(get_option('kbe_breadcrumbs_setting')=='0'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Off','kbe'); ?></span>
+                                    <input type="radio" name="kbe_breadcrumbs_setting" id="kbe_breadcrumbs_setting" value="0" <?php if(get_option('kbe_breadcrumbs_setting')=='0'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Off','kbe'); ?></span>
                                 </td>
                                 <td>&nbsp;</td>
                             </tr>
                             <tr>
                                 <td valign="top">
-                                        <label><?php _e('Knowledgebase home page sidebar','kbe'); ?></label>
+                                    <label><?php _e('Knowledgebase home page sidebar','kbe'); ?></label>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_sidebar_home" id="kbe_sidebar_home" value="1" <?php if(KBE_SIDEBAR_HOME=='1'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Left','kbe'); ?></span>
+                                    <input type="radio" name="kbe_sidebar_home" id="kbe_sidebar_home" value="1" <?php if(KBE_SIDEBAR_HOME=='1'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Left','kbe'); ?></span>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_sidebar_home" id="kbe_sidebar_home" value="2" <?php if(KBE_SIDEBAR_HOME=='2'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Right','kbe'); ?></span>
+                                    <input type="radio" name="kbe_sidebar_home" id="kbe_sidebar_home" value="2" <?php if(KBE_SIDEBAR_HOME=='2'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Right','kbe'); ?></span>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_sidebar_home" id="kbe_sidebar_home" value="0" <?php if(KBE_SIDEBAR_HOME=='0'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('None','kbe'); ?></span>
+                                    <input type="radio" name="kbe_sidebar_home" id="kbe_sidebar_home" value="0" <?php if(KBE_SIDEBAR_HOME=='0'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('None','kbe'); ?></span>
                                 </td>
                             </tr>
                             <tr>
@@ -117,16 +118,16 @@
                                     <label><?php _e('Knowledgebase inner pages sidebar','kbe'); ?></label>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_sidebar_inner" id="kbe_sidebar_inner" value="1" <?php if(KBE_SIDEBAR_INNER=='1'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Left','kbe'); ?></span>
+                                    <input type="radio" name="kbe_sidebar_inner" id="kbe_sidebar_inner" value="1" <?php if(KBE_SIDEBAR_INNER=='1'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Left','kbe'); ?></span>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_sidebar_inner" id="kbe_sidebar_inner" value="2" <?php if(KBE_SIDEBAR_INNER=='2'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Right','kbe'); ?></span>
+                                    <input type="radio" name="kbe_sidebar_inner" id="kbe_sidebar_inner" value="2" <?php if(KBE_SIDEBAR_INNER=='2'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Right','kbe'); ?></span>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_sidebar_inner" id="kbe_sidebar_inner" value="0" <?php if(KBE_SIDEBAR_INNER=='0'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('None','kbe'); ?></span>
+                                    <input type="radio" name="kbe_sidebar_inner" id="kbe_sidebar_inner" value="0" <?php if(KBE_SIDEBAR_INNER=='0'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('None','kbe'); ?></span>
                                 </td>
                             </tr>
                             <tr>
@@ -134,12 +135,12 @@
                                     <label><?php _e('Knowledgebase comments','kbe'); ?></label>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_comments_setting" id="kbe_comments_setting" value="1" <?php if(get_option('kbe_comments_setting')=='1'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('On','kbe'); ?></span>
+                                    <input type="radio" name="kbe_comments_setting" id="kbe_comments_setting" value="1" <?php if(get_option('kbe_comments_setting')=='1'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('On','kbe'); ?></span>
                                 </td>
                                 <td>
-                                <input type="radio" name="kbe_comments_setting" id="kbe_comments_setting" value="0" <?php if(get_option('kbe_comments_setting')=='0'){echo 'checked="checked"';} ?>>
-                                <span><?php _e('Off','kbe'); ?></span>
+                                    <input type="radio" name="kbe_comments_setting" id="kbe_comments_setting" value="0" <?php if(get_option('kbe_comments_setting')=='0'){echo 'checked="checked"';} ?>>
+                                    <span><?php _e('Off','kbe'); ?></span>
                                 </td>
                                 <td>&nbsp;</td>
                             </tr>
@@ -148,7 +149,7 @@
                                     <label><?php _e('Knowledgebase theme color','kbe'); ?></label>
                                 </td>
                                 <td colspan="3">
-                                <input type="text" name="kbe_bgcolor" id="kbe_bgcolor" value="<?php echo get_option('kbe_bgcolor'); ?>" class="cp-field">
+                                    <input type="text" name="kbe_bgcolor" id="kbe_bgcolor" value="<?php echo get_option('kbe_bgcolor'); ?>" class="cp-field">
                                 </td>
                             </tr>
                             <tr>
--- template/archive-kbe_knowledgebase.php
+++ template/archive-kbe_knowledgebase.php
@@ -52,7 +52,8 @@
                $kbe_cat_args = array(
                                     'orderby'       => 'terms_order', 
                                     'order'         => 'ASC',
-                                    'hide_empty'    => true, 
+                                    'hide_empty'    => true,
+                                    'parent'        => 0
                                 );
 
                 $kbe_terms = get_terms(KBE_POST_TAXONOMY, $kbe_cat_args);
@@ -74,6 +75,76 @@
                                 <?php echo $kbe_term_name; ?>
                             </a>
                         </h2>
+                        
+                        <?php
+                            $kbe_child_cat_args = array(
+                                                    'orderby'       => 'terms_order', 
+                                                    'order'         => 'ASC',
+                                                    'parent'        => $kbe_term_id,
+                                                    'hide_empty'    => true, 
+                                                );
+
+                            $kbe_child_terms = get_terms(KBE_POST_TAXONOMY, $kbe_child_cat_args);
+                            
+                            if($kbe_child_terms) {
+                        ?>
+                            <div class="kbe_child_category" style="display: none;">
+                            <?php
+                                foreach($kbe_child_terms as $kbe_child_term){
+                                    $kbe_child_term_id = $kbe_child_term->term_id;
+                                    $kbe_child_term_slug = $kbe_child_term->slug;
+                                    $kbe_child_term_name = $kbe_child_term->name;
+                            ?>
+                                    <h3>
+                                        <span class="kbe_count">
+                                            <?php
+                                                echo $kbe_child_term->count;
+                                                _e(' Articles','kbe');
+                                            ?>
+                                        </span>
+                                        <a href="<?php echo get_term_link($kbe_child_term_slug, 'kbe_taxonomy') ?>">
+                                            <?php echo $kbe_child_term_name; ?>
+                                        </a>
+                                    </h3>
+                                    <ul class="kbe_child_article_list">
+                                <?php
+                                    $kbe_child_post_args = array(
+                                                                'post_type' => KBE_POST_TYPE,
+                                                                'posts_per_page' => KBE_ARTICLE_QTY,
+                                                                'orderby' => 'menu_order',
+                                                                'order' => 'ASC',
+                                                                'tax_query' => array(
+                                                                        array(
+                                                                                'taxonomy' => KBE_POST_TAXONOMY,
+                                                                                'field' => 'term_id',
+                                                                                'terms' => $kbe_child_term_id
+                                                                        )
+                                                                )
+                                                        );
+                                    $kbe_child_post_qry = new WP_Query($kbe_child_post_args);
+                                    if($kbe_child_post_qry->have_posts()) :
+                                        while($kbe_child_post_qry->have_posts()) :
+                                            $kbe_child_post_qry->the_post();
+                                ?>
+                                            <li>
+                                                <a href="<?php the_permalink(); ?>" rel="bookmark">
+                                                    <?php the_title(); ?>
+                                                </a>
+                                            </li>
+                                <?php
+                                        endwhile;
+                                    else :
+                                        echo "No posts";
+                                    endif;
+                                ?>
+                                </ul>
+                            <?php
+                                }
+                            ?>
+                            </div>
+                        <?php
+                            }
+                        ?>
 
                         <ul class="kbe_article_list">
                         <?php
