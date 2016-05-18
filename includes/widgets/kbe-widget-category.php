12eb7a93202b25c36370483cc0cce68382a0e075
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-18 08:49:53 +0200

    Code consistency changes for includes/widgets/kbe-widget-category.php

--- includes/widgets/kbe-widget-category.php
+++ includes/widgets/kbe-widget-category.php
@@ -2,92 +2,92 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /*===============
-    KBE Category Widget
- ===============*/
- 
+	KBE Category Widget
+	===============*/
+
 //========= Custom Knowledgebase Category Widget
 function kbe_category_widgets() {
-    register_widget( 'kbe_Cat_Widget' );
+	register_widget( 'kbe_Cat_Widget' );
 }
 add_action( 'widgets_init', 'kbe_category_widgets' );
 
 //========= Custom Knowledgebase Category Widget Body
 class kbe_Cat_Widget extends WP_Widget {
-	
-    function __construct() {
-        parent::__construct(
-            'kbe_category_widget', // Base ID
-            __( 'Knowledgebase Category', 'kbe' ), // Name
-            array(
-                'description' => __('WP Knowledgebase category widget to show categories on the site', 'kbe'), 
-                'classname' => 'kbe'
-            ) // Args
-        );
-    }
-	
-     //=======> How to display the widget on the screen.
-    function widget($args, $widgetData) {
-		
-        //Our variables from the widget settings.
-        $kbe_widget_cat_title = $widgetData['txtKbeCatHeading'];
-        $kbe_widget_cat_count = $widgetData['txtKbeCatCount'];
-		
-        //=======> widget body
-        echo $args['before_widget'];
-        echo '<div class="kbe_widget">';
-        
-            if ($kbe_widget_cat_title){
-                echo '<h2>'.$kbe_widget_cat_title.'</h2>';
-            }
-			
-            $kbe_cat_args = array(
-                'number' 	=>  $kbe_widget_cat_count,
-                'taxonomy'	=>  'kbe_taxonomy',
-                'orderby'   =>  'terms_order',
-                'order'     =>  'ASC'
-            );
-			
-            $kbe_cats = get_categories($kbe_cat_args);
-            echo "<ul>";
-                foreach($kbe_cats as $kbe_taxonomy){
-                    echo "<li>"
-                            ."<a href=".get_term_link($kbe_taxonomy->slug, 'kbe_taxonomy')." title=".sprintf( __( "View all posts in %s" ), $kbe_taxonomy->name ).">"
-                                .$kbe_taxonomy->name.
-                             "</a>"
-                         ."</li>";
-                }
-            echo "</ul>";
-        
-        echo "</div>";
-        echo $args['after_widget'];
-    }
-	
-    //Update the widget 
-    function update($new_widgetData, $old_widgetData) {
-        $widgetData = $old_widgetData;
-		
-        //Strip tags from title and name to remove HTML 
-        $widgetData['txtKbeCatHeading'] = $new_widgetData['txtKbeCatHeading'];
-        $widgetData['txtKbeCatCount'] = $new_widgetData['txtKbeCatCount'];
-		
-        return $widgetData;
-    }
-    function form($widgetData) {
-        //Set up some default widget settings.
-        $widgetData = wp_parse_args((array) $widgetData, array(
-            'txtKbeCatHeading' => '',
-            'txtKbeCatCount' => 10,
-        ) );
+
+	function __construct() {
+		parent::__construct(
+			'kbe_category_widget', // Base ID
+			__( 'Knowledgebase Category', 'kbe' ), // Name
+			array(
+				'description' => __( 'WP Knowledgebase category widget to show categories on the site', 'kbe' ),
+				'classname'   => 'kbe'
+			) // Args
+		);
+	}
+
+	//=======> How to display the widget on the screen.
+	function widget( $args, $widgetData ) {
+		//Our variables from the widget settings.
+		$kbe_widget_cat_title = $widgetData['txtKbeCatHeading'];
+		$kbe_widget_cat_count = $widgetData['txtKbeCatCount'];
+
+		//=======> widget body
+		echo $args['before_widget'];
+		echo '<div class="kbe_widget">';
+
+			if ( $kbe_widget_cat_title ) {
+				echo '<h2>' . $kbe_widget_cat_title . '</h2>';
+			}
+
+			$kbe_cat_args = array(
+				'number'   =>  $kbe_widget_cat_count,
+				'taxonomy' =>  'kbe_taxonomy',
+				'orderby'  =>  'terms_order',
+				'order'    =>  'ASC'
+			);
+
+			$kbe_cats = get_categories( $kbe_cat_args );
+			echo '<ul>';
+				foreach ( $kbe_cats as $kbe_taxonomy ) {
+					echo '<li>'
+							.'<a href=' . get_term_link( $kbe_taxonomy->slug, 'kbe_taxonomy' ) . ' title=' . sprintf( __( 'View all posts in %s' ), $kbe_taxonomy->name ) . '>'
+								.$kbe_taxonomy->name .
+							'</a>'
+						.'</li>';
+				}
+			echo '</ul>';
+
+		echo '</div>';
+		echo $args['after_widget'];
+	}
+
+	//Update the widget
+	function update( $new_widgetData, $old_widgetData ) {
+		$widgetData = $old_widgetData;
+
+		//Strip tags from title and name to remove HTML
+		$widgetData['txtKbeCatHeading'] = $new_widgetData['txtKbeCatHeading'];
+		$widgetData['txtKbeCatCount']   = $new_widgetData['txtKbeCatCount'];
+
+		return $widgetData;
+	}
+
+	function form( $widgetData ) {
+		//Set up some default widget settings.
+		$widgetData = wp_parse_args( (array) $widgetData, array(
+			'txtKbeCatHeading' => '',
+			'txtKbeCatCount'   => 10,
+		) );
 ?>
         <p>
-            <label for="<?php echo $this->get_field_id('txtKbeCatHeading'); ?>"><?php _e('Title:') ?></label>
-            <input id="<?php echo $this->get_field_id('txtKbeCatHeading'); ?>" class="widefat" name="<?php echo $this->get_field_name('txtKbeCatHeading'); ?>" value="<?php echo $widgetData['txtKbeCatHeading']; ?>" />
-        </p>    
+            <label for="<?php echo $this->get_field_id( 'txtKbeCatHeading' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input id="<?php echo $this->get_field_id( 'txtKbeCatHeading' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'txtKbeCatHeading' ); ?>" value="<?php echo $widgetData['txtKbeCatHeading']; ?>" />
+        </p>
         <p>
-            <label for="<?php echo $this->get_field_id('txtKbeCatCount'); ?>"><?php _e('Category Quantity:','kbe'); ?></label>
-            <input id="<?php echo $this->get_field_id('txtKbeCatCount'); ?>" name="<?php echo $this->get_field_name('txtKbeCatCount'); ?>" value="<?php echo $widgetData['txtKbeCatCount']; ?>" />
+            <label for="<?php echo $this->get_field_id( 'txtKbeCatCount' ); ?>"><?php _e( 'Category Quantity:', 'kbe' ); ?></label>
+            <input id="<?php echo $this->get_field_id( 'txtKbeCatCount' ); ?>" name="<?php echo $this->get_field_name( 'txtKbeCatCount' ); ?>" value="<?php echo $widgetData['txtKbeCatCount']; ?>" />
         </p>
 <?php
-    }
+	}
+
 }
-?>
