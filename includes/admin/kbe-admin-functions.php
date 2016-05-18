95b9ce74f53f3a08a99a891dfa02ebca70c57597
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-18 08:49:35 +0200

    Code consistency changes for includes/admin/kbe-admin-functions.php

--- includes/admin/kbe-admin-functions.php
+++ includes/admin/kbe-admin-functions.php
@@ -3,51 +3,48 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
 //=========> Enqueue color picker
-function kbe_admin_scripts($hook_suffix) {
-
+function kbe_admin_scripts( $hook_suffix ) {
 	// Settings page
 	if ( $hook_suffix == 'kbe_knowledgebase_page_kbe_options' ) {
-		wp_enqueue_style('wp-color-picker');
-		wp_enqueue_script('cp-script-handle', WP_KNOWLEDGEBASE.'/assets/js/color_picker.js', array( 'wp-color-picker' ), false, true);
+		wp_enqueue_style( 'wp-color-picker' );
+		wp_enqueue_script( 'cp-script-handle', WP_KNOWLEDGEBASE . '/assets/js/color_picker.js', array( 'wp-color-picker' ), false, true );
 	}
 
 	// Order page
 	if ( $hook_suffix == 'kbe_knowledgebase_page_kbe_order' ) {
-		wp_enqueue_script("jquery-ui-sortable");
+		wp_enqueue_script( 'jquery-ui-sortable' );
 	}
 
-	wp_register_style('kbe_admin_css', WP_KNOWLEDGEBASE.'/assets/css/kbe-admin-style.css');
+	wp_register_style( 'kbe_admin_css', WP_KNOWLEDGEBASE . '/assets/css/kbe-admin-style.css' );
 	if ( isset( $_GET['post_type'] ) && $_GET['post_type'] == 'kbe_knowledgebase' ) {
-		wp_enqueue_style('kbe_admin_css');
+		wp_enqueue_style( 'kbe_admin_css' );
 	}
-
 }
-add_action('admin_enqueue_scripts', 'kbe_admin_scripts');
+add_action( 'admin_enqueue_scripts', 'kbe_admin_scripts' );
 
 //=========> Plugin menu
 function kbe_plugin_menu() {
-	add_submenu_page('edit.php?post_type=kbe_knowledgebase', 'Order', 'Order', 'manage_options', 'kbe_order', 'wp_kbe_order');
-	add_submenu_page('edit.php?post_type=kbe_knowledgebase', 'Settings', 'Settings', 'manage_options', 'kbe_options', 'wp_kbe_options');
+	add_submenu_page( 'edit.php?post_type=kbe_knowledgebase', 'Order', 'Order', 'manage_options', 'kbe_order', 'wp_kbe_order' );
+	add_submenu_page( 'edit.php?post_type=kbe_knowledgebase', 'Settings', 'Settings', 'manage_options', 'kbe_options', 'wp_kbe_options' );
 }
-add_action('admin_menu', 'kbe_plugin_menu');
+add_action( 'admin_menu', 'kbe_plugin_menu' );
 
 //  Require File kbe_order.php
-function wp_kbe_order(){
-	require dirname( __FILE__ ) . "/../kbe-order.php";
+function wp_kbe_order() {
+	require dirname( __FILE__ ) . '/../kbe-order.php';
 }
 
 //=========> Require Files
 //  kbe_settings.php
-function wp_kbe_options(){
-	require "kbe-settings.php";
+function wp_kbe_options() {
+	require 'kbe-settings.php';
 }
 
 //=========> Register plugin settings
 function kbe_register_settings() {
 	register_setting( 'kbe_settings', 'kbe_settings', 'kbe_validate_settings' );
 }
-add_action('admin_init', 'kbe_register_settings');
-
+add_action( 'admin_init', 'kbe_register_settings' );
 
 /**
  * Sanitize and validate plugin settings
@@ -59,12 +56,12 @@ function kbe_validate_settings( $input ) {
 	$input['kbe_plugin_slug'] = isset( $input['kbe_plugin_slug'] ) ? sanitize_title( $input['kbe_plugin_slug'] ) : '';
 	$input['kbe_article_qty'] = intval( $input['kbe_article_qty'] );
 
-	$input['kbe_search_setting'] =  isset( $input['kbe_search_setting'] ) && $input['kbe_search_setting'] ? 1 : 0 ;
+	$input['kbe_search_setting']      =  isset( $input['kbe_search_setting'] ) && $input['kbe_search_setting'] ? 1 : 0 ;
 	$input['kbe_breadcrumbs_setting'] =  isset( $input['kbe_breadcrumbs_setting'] ) && $input['kbe_breadcrumbs_setting'] ? 1 : 0 ;
 
 	$sidebar_positions = array( 0, 1, 2 );
 
-	$input['kbe_sidebar_home'] = isset( $input['kbe_sidebar_home'] ) && in_array( $input['kbe_sidebar_home'], $sidebar_positions ) ? intval( $input['kbe_sidebar_home'] ) : 0;
+	$input['kbe_sidebar_home']  = isset( $input['kbe_sidebar_home'] ) && in_array( $input['kbe_sidebar_home'], $sidebar_positions ) ? intval( $input['kbe_sidebar_home'] ) : 0;
 	$input['kbe_sidebar_inner'] = isset( $input['kbe_sidebar_inner'] ) && in_array( $input['kbe_sidebar_inner'], $sidebar_positions ) ? intval( $input['kbe_sidebar_inner'] ) : 0;
 
 	$input['kbe_comments_setting'] =  isset( $input['kbe_comments_setting'] ) && $input['kbe_comments_setting'] ? 1 : 0 ;
@@ -74,21 +71,19 @@ function kbe_validate_settings( $input ) {
 	return $input;
 }
 
-
 //=========>  KBE Custom Taxonomy Order
-function kbe_tax_order($orderby, $args){
-	$kbe_tax = "kbe_taxonomy";
+function kbe_tax_order( $orderby, $args ) {
+	$kbe_tax = 'kbe_taxonomy';
 
-	if($args['orderby'] == 'terms_order'){
+	if ( $args['orderby'] == 'terms_order' ) {
 		return 't.terms_order';
-	}elseif($kbe_tax == 1 && !isset($_GET['orderby'])){
+	} elseif ( $kbe_tax == 1 && ! isset( $_GET['orderby'] ) ) {
 		return 't.terms_order';
-	}else{
+	} else {
 		return $orderby;
 	}
 }
-add_filter('get_terms_orderby', 'kbe_tax_order', 10, 2);
-
+add_filter( 'get_terms_orderby', 'kbe_tax_order', 10, 2 );
 
 function kbe_migrations_check() {
 	require_once plugin_dir_path( __FILE__ ) . '../migrations/class-migration-manager.php';
